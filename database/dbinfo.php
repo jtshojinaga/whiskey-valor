f329ad35234168dd08116d3c4b828daa395eeb7a
ivaebhin <strangeone.pony@gmail.com>
2025-09-22 12:05:58 -0400

    Updated database login information.

--- database/dbinfo.php
+++ database/dbinfo.php
@@ -20,9 +20,9 @@ date_default_timezone_set('America/New_York');
 
 function connect() {
     $host = "localhost"; 
-    $database = "spcadb";
-    $user = "spcadb";
-    $pass = "spcadb";
+    $database = "whiskeydb";
+    $user = "whiskeydb";
+    $pass = "whiskeydb";
     if ($_SERVER['SERVER_NAME'] == 'jenniferp160.sg-host.com') {
         $user = 'uknrzrk8sj1e7';
         $database = 'dbkzrh4cfmxbt0';
