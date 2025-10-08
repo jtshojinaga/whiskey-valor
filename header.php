9a30c34cf4a6cf9261385fadeaebf48fa93015f7
ForgedGamer <33134376+ForgedGamer@users.noreply.github.com>
2025-10-08 12:50:07 -0400

    skeleton of view filtering header annotation
    Added skeleton of view filtering and annotated the header file over some specific functionality that may be helpful

--- header.php
+++ header.php
@@ -591,50 +591,54 @@ if (date("H:i:s") > "18:19:59") {
          */
         //pages guests are allowed to view
         // LOWERCASE
-        $permission_array['index.php'] = 0;
-        $permission_array['about.php'] = 0;
-        $permission_array['apply.php'] = 0;
-        $permission_array['logout.php'] = 0;
-        $permission_array['volunteerregister.php'] = 0;
-	$permission_array['leaderboard.php'] = 0;
+        /*
+        *  For A guest can log in, go to WVF's home page,  
+        * -Evan
+        */
+        $permission_array['index.php'] = 0; // WVF Home page
+        $permission_array['about.php'] = 0; //WVF - Not able to directly access - Likely just need to re-route to 
+        $permission_array['apply.php'] = 0; //WVF - Not able to directly access
+        $permission_array['logout.php'] = 0; //WVF - Logout page ain
+        $permission_array['volunteerregister.php'] = 0; //WVF - Alter to registering for account
+	    $permission_array['leaderboard.php'] = 0; //WVF - Probably get rid of this guy
         // $permission_array['findanimal.php'] = 0; //TODO DELETE
         //pages volunteers can view
         $permission_array['help.php'] = 1;
-        $permission_array['dashboard.php'] = 1;
-        $permission_array['calendar.php'] = 1;
-        $permission_array['eventsearch.php'] = 1;
+        $permission_array['dashboard.php'] = 1; //WVF - Might be good to alter this for registered users to be able to see registered events and where they can edit user info 
+        $permission_array['calendar.php'] = 1; //WVF - Everyone can see this
+        $permission_array['eventsearch.php'] = 1; 
         $permission_array['changepassword.php'] = 1;
-        $permission_array['editprofile.php'] = 1;
-        $permission_array['inbox.php'] = 1;
-        $permission_array['date.php'] = 1;
-        $permission_array['event.php'] = 1;
+        $permission_array['editprofile.php'] = 1; //WVF - Repurpose for SCRUM-5
+        $permission_array['inbox.php'] = 1; //WVF - Not for registered users, since they want emails. But would be good for 'suggestions' for ADMINS to see 
+        $permission_array['date.php'] = 1; 
+        $permission_array['event.php'] = 1; 
         $permission_array['viewprofile.php'] = 1;
         $permission_array['viewnotification.php'] = 1;
-        $permission_array['volunteerreport.php'] = 1;
+        $permission_array['volunteerreport.php'] = 1; //WVF - Attendance Report?
         $permission_array['viewmyupcomingevents.php'] = 1;
-        $permission_array['volunteerviewgroup.php'] = 1;
+        $permission_array['volunteerviewgroup.php'] = 1; 
 	    $permission_array['viewcheckinout.php'] = 1;
         $permission_array['viewresources.php'] = 1;
         $permission_array['discussionmain.php'] = 1;
-        $permission_array['viewdiscussions.php'] = 1;
-        $permission_array['discussioncontent.php'] = 1;
+        $permission_array['viewdiscussions.php'] = 1; //WVF - Edit discussions for suggestions?
+        $permission_array['discussioncontent.php'] = 1; //WVF - Edit discussions for suggestions?
         $permission_array['milestonepoints.php'] = 1;
         $permission_array['selectvotm.php'] = 1;
         $permission_array['volunteerviewgroupmembers.php'] = 1;
         //pages only managers can view
-        $permission_array['viewallevents.php'] = 0;
+        $permission_array['viewallevents.php'] = 0; //WVF - For admins to do view 
         $permission_array['personsearch.php'] = 2;
         $permission_array['personedit.php'] = 0; // changed to 0 so that applicants can apply
         $permission_array['viewschedule.php'] = 2;
         $permission_array['addweek.php'] = 2;
         $permission_array['log.php'] = 2;
         $permission_array['reports.php'] = 2;
-        $permission_array['eventedit.php'] = 2;
+        $permission_array['eventedit.php'] = 2; //WVF - TODO: Evaluated differenced between eventedit and editevent.
         $permission_array['modifyuserrole.php'] = 2;
-        $permission_array['addevent.php'] = 2;
-        $permission_array['editevent.php'] = 2;
+        $permission_array['addevent.php'] = 2; //WVF - Admin Event work!
+        $permission_array['editevent.php'] = 2; //WVF - Admin Event work!
         // $permission_array['roster.php'] = 2; //TODO DELETE
-        $permission_array['report.php'] = 2;
+        $permission_array['report.php'] = 2; // WVF TODO: Look to see how these reports can be reworked to do attendance report
         $permission_array['reportspage.php'] = 2;
         $permission_array['resetpassword.php'] = 2;
         // $permission_array['addappointment.php'] = 2; //TODO DELETE
@@ -669,7 +673,7 @@ if (date("H:i:s") > "18:19:59") {
         $permission_array['clockoutbulk.php'] = 2;
         $permission_array['clockOut.php'] = 2;
         $permission_array['edithours.php'] = 2;
-        $permission_array['eventlist.php'] = 1;
+        $permission_array['eventlist.php'] = 1;   
         $permission_array['eventsignup.php'] = 1;
         $permission_array['eventfailure.php'] = 1;
         $permission_array['signupsuccess.php'] = 1;
