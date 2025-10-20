a5bc0ee2f09d1bd4170d3b0c1608d2ee0c0ff561
Kassandra Williams <kassandra.williams.22@cnu.edu>
2025-10-19 23:50:08 -0400

    Can view weekly and daily views by url

--- calendar.php
+++ calendar.php
@@ -15,48 +15,89 @@
         echo "Not Logged in";
     }
 
-    // Redirect to current month
-    if (!isset($_GET['month'])) {
-        $month = date("Y-m");
-    } else {
+
+    $view = 'month';
+    if (isset($_GET['week'])) {
+        $view = 'week';
+        $week = $_GET['week']; // format: YYYY-MM-DD
+    } elseif (isset($_GET['day'])) {
+        $view = 'day';
+        $day = $_GET['day']; // format: YYYY-MM-DD
+    } elseif (isset($_GET['month'])) {
         $month = $_GET['month'];
+    } else {
+        $month = date("Y-m");
     }
     
 
-    $year = substr($month, 0, 4);
-    $month2digit = substr($month, 5, 2);
-
-    $timestamp = strtotime($date);
-    $today = strtotime(date("Y-m-d"));
-
- 
-
-    $first = $month . '-01';
-    // Convert to date
-    $month = strtotime($month);
-    // Find first day of the month
-    $first = strtotime($first);
-    // Find previous and next month
-    $previousMonth = strtotime(date('Y-m', $month) . ' -1 month');
-    $nextMonth = strtotime(date('Y-m', $month) . ' +1 month');
-    // Validate; redirect if bad arg given
-    if (!$month) {
-        header('Location: calendar.php?month=' . date("Y-m"));
-        die();
-    }
-    $calendarStart = $first;
-    // Back up until we find the first Sunday that should appear on the calendar
-    while (date('w', $calendarStart) > 0) {
-        $calendarStart = strtotime(date('Y-m-d', $calendarStart) . ' -1 day');
-    }
-    $calendarEnd = date('Y-m-d', strtotime(date('Y-m-d', $calendarStart) . ' +34 day'));
-    $calendarEndEpoch = strtotime($calendarEnd);
-    $weeks = 5;
-    // Add another row if it's needed to display all days in the month
-    if (date('m', strtotime($calendarEnd . ' +1 day')) == date('m', $first)) {
-        $calendarEnd = date('Y-m-d', strtotime($calendarEnd . ' +7 day'));
+    if ($view === 'month') {
+        $year = substr($month, 0, 4);
+        $month2digit = substr($month, 5, 2);
+        $timestamp = strtotime($date);
+        $today = strtotime(date("Y-m-d"));
+        $first = $month . '-01';
+        // Convert to date
+        $month = strtotime($month);
+        // Find first day of the month
+        $first = strtotime($first);
+        // Find previous and next month
+        $previousMonth = strtotime(date('Y-m', $month) . ' -1 month');
+        $nextMonth = strtotime(date('Y-m', $month) . ' +1 month');
+        // Validate; redirect if bad arg given
+        if (!$month) {
+            header('Location: calendar.php?month=' . date("Y-m"));
+            die();
+        }
+        $calendarStart = $first;
+        // Back up until we find the first Sunday that should appear on the calendar
+        while (date('w', $calendarStart) > 0) {
+            $calendarStart = strtotime(date('Y-m-d', $calendarStart) . ' -1 day');
+        }
+        $calendarEnd = date('Y-m-d', strtotime(date('Y-m-d', $calendarStart) . ' +34 day'));
         $calendarEndEpoch = strtotime($calendarEnd);
-        $weeks = 6;
+        $weeks = 5;
+        // Add another row if it's needed to display all days in the month
+        if (date('m', strtotime($calendarEnd . ' +1 day')) == date('m', $first)) {
+            $calendarEnd = date('Y-m-d', strtotime($calendarEnd . ' +7 day'));
+            $calendarEndEpoch = strtotime($calendarEnd);
+            $weeks = 6;
+        }
+
+    }
+        
+    elseif ($view === 'week') {
+        $week = strtotime($week);
+
+
+        $year = substr($date, 0, 4);
+
+
+        $month = substr($date, 0, 7);
+        $month = strtotime($month);
+        $previousMonth = strtotime(date('Y-m', $month) . ' -1 month');
+        $nextMonth = strtotime(date('Y-m', $month) . ' +1 month');
+
+        // $timestamp = strtotime($date);
+        $today = strtotime(date("Y-m-d"));
+        $calendarStart = $week;
+        $calendarEnd = date('Y-m-d', strtotime(date('Y-m-d', $calendarStart) . ' +7 day'));
+        $calendarEndEpoch = strtotime($calendarEnd);
+    }
+
+    elseif ($view === 'day') {
+        $day = strtotime($day);
+        $year = substr($date, 0, 4);
+        $month = substr($date, 0, 7);
+        $month = strtotime($month);
+        $previousMonth = strtotime(date('Y-m', $month) . ' -1 month');
+        $nextMonth = strtotime(date('Y-m', $month) . ' +1 month');
+        
+        $timestamp = strtotime($date);
+        $today = strtotime(date("Y-m-d"));
+        $calendarStart = $day;
+        $calendarEnd = date('Y-m-d', $calendarStart);
+        $calendarEndEpoch = strtotime($calendarEnd);
+
     }
 ?>
 <!DOCTYPE html>
@@ -102,7 +143,13 @@
         <main class="calendar-view">
             <h1 class='calendar-header' style="height: 75px;">
                 <img id="previous-month-button" src="images/arrow-back.png" data-month="<?php echo date("Y-m", $previousMonth); ?>">
-                <span id="calendar-heading-month" style="font-weight: 700; font-size: 36px;">Events - <?php echo date('F Y', $month); ?></span>
+                <?php if ($view === 'month') : ?>
+                    <span id="calendar-heading-month" style="font-weight: 700; font-size: 36px;">Events - <?php echo date('F Y', $month); ?></span>
+                <?php elseif ($view ==='week') : ?>
+                    <span id="calendar-heading-month" style="font-weight: 700; font-size: 36px;">Events - Week of <?php echo date('F j, Y', $week); ?></span>
+                <?php elseif ($view ==='day') : ?>
+                    <span id="calendar-heading-month" style="font-weight: 700; font-size: 36px;">Events - <?php echo date('F j, Y', $day); ?></span>
+                <?php endif ?>
                 <img id="next-month-button" src="images/arrow-forward.png" data-month="<?php echo date("Y-m", $nextMonth); ?>">
             </h1>
             <!-- <input type="date" id="month-jumper" value="<?php echo date('Y-m-d', $month); ?>" min="2023-01-01"> -->
@@ -116,31 +163,107 @@
                 <div class="happy-toast">Event canceled successfully.</div>
             <?php endif ?>
             <div class="table-wrapper">
-                <table id="calendar">
-                    <thead>
-                        <tr>
-                            <th>Sunday</th>
-                            <th>Monday</th>
-                            <th>Tuesday</th>
-                            <th>Wednesday</th>
-                            <th>Thursday</th>
-                            <th>Friday</th>
-                            <th>Saturday</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    <?php
-                        $date = $calendarStart;
-                        $start = date('Y-m-d', $calendarStart);
-                        $end = date('Y-m-d', $calendarEndEpoch);
-                        require_once('database/dbEvents.php');
-                        // HERE we want to fetch ALL EVENTS for logged in users
-                        // but users who are not logged in should only be fetching public events
-                        $events = fetch_events_in_date_range($start, $end, $loggedIn);
-                        for ($week = 0; $week < $weeks; $week++) {
-                            echo '
-                                <tr class="calendar-week">
-                            ';
+                <?php if ($view === 'month') :?>
+                    <table id="calendar">
+                        <thead>
+                            <tr>
+                                <th>Sunday</th>
+                                <th>Monday</th>
+                                <th>Tuesday</th>
+                                <th>Wednesday</th>
+                                <th>Thursday</th>
+                                <th>Friday</th>
+                                <th>Saturday</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                            $date = $calendarStart;
+                            $start = date('Y-m-d', $calendarStart);
+                            $end = date('Y-m-d', $calendarEndEpoch);
+                            require_once('database/dbEvents.php');
+                            // HERE we want to fetch ALL EVENTS for logged in users
+                            // but users who are not logged in should only be fetching public events
+                            $events = fetch_events_in_date_range($start, $end, $loggedIn);
+                            for ($week = 0; $week < $weeks; $week++) {
+                                echo '
+                                    <tr class="calendar-week">
+                                ';
+                                for ($day = 0; $day < 7; $day++) {
+                                    $extraAttributes = '';
+                                    $extraClasses = '';
+                                    if ($date == $today) {
+                                        $extraClasses = ' today';
+                                    }
+                                    if (date('m', $date) != date('m', $month)) {
+                                        $extraClasses .= ' other-month';
+                                        $extraAttributes .= ' data-month="' . date('Y-m', $date) . '"';
+                                    }
+                                    $eventsStr = '';
+                                    $e = date('Y-m-d', $date);
+
+                                    if (isset($events[$e])) {
+                                        $dayEvents = $events[$e];
+                                        foreach ($dayEvents as $info) {
+
+                                            $backgroundCol = '#996d49ff'; // default color
+
+                                            if(isset($_SESSION['access_level'])) {
+                                                if (is_archived($info['id'])) { // archived event
+                                                    if ($_SESSION['access_level'] < 2) {
+                                                        continue; // users cannot see archived events
+                                                    }
+                                                    $backgroundCol = '#aaaaaa'; //TODO
+
+                                                } elseif (check_if_signed_up($info['id'], $_SESSION['_id'])) {// user is signed-up for event
+                                                    $backgroundCol = '#4CAF50';
+
+                                                }
+                                                $eventsStr .= '<a class="calendar-event" style="background-color: ' . $backgroundCol . '" href="event.php?id=' . $info['id'] . '&user_id=' . $_SESSION['_id'] . '">' . htmlspecialchars_decode($info['name']) . '</a>';
+
+                                            } else {
+                                                $eventsStr .= '<a class="calendar-event" style="background-color: ' . $backgroundCol . '" href="event.php?id=' . $info['id'] . '&user_id=guest' . '">' . htmlspecialchars_decode($info['name']) . '</a>';
+                                            }
+                                            
+                                        }
+                                    }
+                                    echo '<td class="calendar-day' . $extraClasses . '" ' . $extraAttributes . ' data-date="' . date('Y-m-d', $date) . '">
+                                        <div class="calendar-day-wrapper">
+                                            <p class="calendar-day-number">' . date('j', $date) . '</p>
+                                            ' . $eventsStr . '
+                                        </div>
+                                    </td>';
+                                    $date = strtotime(date('Y-m-d', $date) . ' +1 day');
+                                }
+                                echo '
+                                    </tr>';
+                            }
+                        ?>
+                        </tbody>
+                    </table>
+                <?php elseif ($view === 'week') : ?>
+                    <table class="calendar-week">
+                        <thead>
+                            <tr>
+                                <th>Sunday</th>
+                                <th>Monday</th>
+                                <th>Tuesday</th>
+                                <th>Wednesday</th>
+                                <th>Thursday</th>
+                                <th>Friday</th>
+                                <th>Saturday</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                            $date = $calendarStart;
+
+                            $start = date('Y-m-d', $calendarStart);
+                            $end = date('Y-m-d', $calendarEndEpoch);
+                            require_once('database/dbEvents.php');
+                            // HERE we want to fetch ALL EVENTS for logged in users
+                            // but users who are not logged in should only be fetching public events
+                            $events = fetch_events_in_date_range($start, $end, $loggedIn);
                             for ($day = 0; $day < 7; $day++) {
                                 $extraAttributes = '';
                                 $extraClasses = '';
@@ -189,11 +312,74 @@
                             }
                             echo '
                                 </tr>';
-                        }
-                    ?>
-                    </tbody>
-                </table>
+                        ?>
+                        </tbody>
+                    </table>
             </div>
+                <?php elseif ($view === 'day') : ?>
+                    <table>
+                        <tbody>
+                        <?php
+                            $date = $calendarStart;
+                            $start = date('Y-m-d', $calendarStart);
+                            $end = date('Y-m-d', $calendarEndEpoch);
+                            require_once('database/dbEvents.php');
+                            // HERE we want to fetch ALL EVENTS for logged in users
+                            // but users who are not logged in should only be fetching public events
+                            $events = fetch_events_in_date_range($start, $end, $loggedIn);
+
+                            $extraAttributes = '';
+                            $extraClasses = '';
+                            if ($date == $today) {
+                                $extraClasses = ' today';
+                            }
+                            if (date('m', $date) != date('m', $month)) {
+                                $extraClasses .= ' other-month';
+                                $extraAttributes .= ' data-month="' . date('Y-m', $date) . '"';
+                            }
+                            $eventsStr = '';
+                            $e = date('Y-m-d', $date);
+
+                            if (isset($events[$e])) {
+                                $dayEvents = $events[$e];
+                                foreach ($dayEvents as $info) {
+
+                                    $backgroundCol = '#996d49ff'; // default color
+
+                                    if(isset($_SESSION['access_level'])) {
+                                        if (is_archived($info['id'])) { // archived event
+                                            if ($_SESSION['access_level'] < 2) {
+                                                continue; // users cannot see archived events
+                                            }
+                                            $backgroundCol = '#aaaaaa'; //TODO
+
+                                        } elseif (check_if_signed_up($info['id'], $_SESSION['_id'])) {// user is signed-up for event
+                                            $backgroundCol = '#4CAF50';
+
+                                        }
+                                        $eventsStr .= '<a class="calendar-event" style="background-color: ' . $backgroundCol . '" href="event.php?id=' . $info['id'] . '&user_id=' . $_SESSION['_id'] . '">' . htmlspecialchars_decode($info['name']) . '</a>';
+
+                                    } else {
+                                        $eventsStr .= '<a class="calendar-event" style="background-color: ' . $backgroundCol . '" href="event.php?id=' . $info['id'] . '&user_id=guest' . '">' . htmlspecialchars_decode($info['name']) . '</a>';
+                                    }
+                                    
+                                }
+                            }
+                            echo '<td class="calendar-day' . $extraClasses . '" ' . $extraAttributes . ' data-date="' . date('Y-m-d', $date) . '">
+                                <div class="calendar-day-wrapper">
+                                    <p class="calendar-day-number">' . date('j', $date) . '</p>
+                                    ' . $eventsStr . '
+                                </div>
+                            </td>';
+                            $date = strtotime(date('Y-m-d', $date) . ' +1 day');
+
+                            echo '
+                                </tr>';
+                        ?>
+                        </tbody>
+                    </table>
+                <?php endif ?>
+            
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">            
             
             <?php
