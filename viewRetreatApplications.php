a9b792b1c62d6b4f8949ee73b3c83dd9f224b095
ivaebhin <jtakeshishojinaga@gmail.com>
2025-10-27 14:50:58 -0400

    Finished viewRetreatApplications skeleton for Kass

--- viewRetreatApplications.php
+++ viewRetreatApplications.php
@@ -1,3 +1,33 @@
 <?php 
-    // Will eventually have stuff here! ~Blue
+    session_cache_expire(30);
+    session_start();
+    ini_set("display_errors",1);
+    error_reporting(E_ALL);
+    if(!isset($_SESSION['_id'])) {
+        header('Location: login.php');
+        die();
+    }
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php require_once('universal.inc'); ?>
+        <title>Whiskey Valor | View Retreat Applications</title>
+        <link src="css/base.css" rel="stylesheet">
+    </head>
+    <body>
+        <?php require_once('header.php'); 
+        $isAdmin = $_SESSION['access_level'] >= 2;
+
+        if(!$isAdmin): ?> <!-- With permission array set this should be redundant -->
+            <div class="error-toast">You do not have permission to view this page.</div></body>
+        <?php else: ?>
+            <div class="applications-container">
+                <div class="application-card">
+                    <!-- feel free to change or use these as you like -->
+                </div>
+            </div>
+        <?php endif ?>
+    </body>
+</html>
